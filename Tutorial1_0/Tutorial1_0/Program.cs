a9f98fbc96d44d98b85a5050680c80ea424ac456
Tor Kay <65842590+torkay@users.noreply.github.com>
2023-02-08 22:31:21 +1000

    Reverse Guessing Game

---
+++ Tutorial1_0/Tutorial1_0/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rangeLow = 1;
+        int rangeHigh = 101;
+
+        bool playGame = true;
+        bool playFair = true;
+
+        var random = new Random();
+
+        while (playGame && playFair == true)
+        {
+            if (rangeHigh - rangeLow == 0)
+            {
+                Console.WriteLine("Wait a second... You're not playing fair!");
+                playFair = false;
+                break;
+            }
+
+            int guess = random.Next(rangeLow, rangeHigh);
+
+            Console.WriteLine($"Hmm... I think {guess}\nAm I: 'correct', 'too high', or 'too low'");
+            string response = null;
+
+            bool safetyNet = true;
+
+            while (safetyNet == true)
+            {
+                response = Console.ReadLine();
+
+                if (response.Equals("correct") || response.Equals("too high") || response.Equals("too low"))
+                {
+                    safetyNet = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid response, please re-read my question and try again!");
+                }
+            }
+
+            switch (response)
+            {
+                case "correct":
+                    Console.WriteLine("Yay! I always win.");
+                    playGame = false;
+                    break;
+
+                case "too high":
+                    rangeHigh = guess - 1;
+                    break;
+
+                case "too low":
+                    rangeLow = guess + 1;
+                    break;
+            }
+        }
+        Console.WriteLine("Press any key to close the program");
+        Console.ReadKey();
+    }
+}
